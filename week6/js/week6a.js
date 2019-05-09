396974d0987d7ab438d2e0ee6fc8f1d77259ba7c
syedahmedhaidershah <sahsh9996@yahoo.com>
2019-05-09 07:51:54 +0500

    roza3-post-sunrise-commit

---
+++ week6/js/week6a.js
@@ -0,0 +1,13 @@
+const week6 = {
+    g: {
+        draggableShape: []
+    },
+    init: () => {
+        document.addEventListener('DOMContentLoaded', week6.proceed, false);
+    },
+    proceed: () => {
+
+    }
+}
+
+week6.init();
