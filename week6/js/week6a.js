a9d8ff15f363762daa061ad08403d23b6a1b736f
syedahmedhaidershah <sahsh9996@yahoo.com>
2019-05-09 09:07:50 +0500

    week6-finalized

--- week6/js/week6a.js
+++ week6/js/week6a.js
@@ -5,7 +5,12 @@ const wsix = {
     },
     v: {
         inPlace: 0,
-        dragTemp: null
+        dragTemp: null,
+        filled: {
+            square: false,
+            triangle: false,
+            circle: false
+        }
     },
     init: () => {
         document.addEventListener('DOMContentLoaded', wsix.proceed, false);
@@ -37,6 +42,9 @@ const wsix = {
     setListeners() {
         wsix.g.draggableShape.forEach((v) => {
             try {
+                v.ontouchstart = ($e) => {
+                    wsix.v.dragTemp = $e.target;
+                };
                 v.ondragstart = ($e) => {
                     wsix.v.dragTemp = $e.target;
                 };
@@ -49,6 +57,50 @@ const wsix = {
                     let receivingElement = wsix.getShapeSetEl(event.target);
                     let rAttr = receivingElement.getAttribute('shapeset');
                     let dtAttr = wsix.v.dragTemp.getAttribute('shapeset');
+                    if(wsix.v.filled[rAttr]) {
+                        return false;
+                    } else {
+                        wsix.v.filled[rAttr] = true;
+                    }
+                    if(rAttr == dtAttr) {
+                        const useClass = receivingElement.children[0].className;
+                        receivingElement.children[0].className += ' d-none';
+                        wsix.v.dragTemp.className += ' d-none';
+                        document.querySelector('.final-container.'.concat(rAttr)).className = rAttr.concat(' final-container');
+                        wsix.v.inPlace += 1;
+                    } else {
+                        const useClass = receivingElement.children[0].className;
+                        receivingElement.children[0].className = useClass.replace(/(d-none)/g,'');
+                    }
+                    if(wsix.v.inPlace > 2) {
+                        setTimeout(() => {
+                            Array.from(document.querySelectorAll('*')).forEach(el => {
+                                el.className += ' fade-to-green';
+                            });
+                            const then = new Date().getTime();
+                            const remBorderInterval = setInterval(() => {
+                                const now = new Date().getTime();
+                                if(now - then > 2700) {
+                                    Array.from(document.getElementsByClassName('square-container')).forEach(el => {
+                                        el.className = '';
+                                    });
+                                    clearInterval(remBorderInterval);
+                                }
+                            }, 16);
+                        }, 300);
+                    }
+                    event.preventDefault();
+                }
+                v.touchend = ($e) => {
+                    if (wsix.v.dragTemp == null) return;
+                    let receivingElement = wsix.getShapeSetEl(event.target);
+                    let rAttr = receivingElement.getAttribute('shapeset');
+                    let dtAttr = wsix.v.dragTemp.getAttribute('shapeset');            
+                    if(wsix.v.filled[rAttr]) {
+                        return false;
+                    } else {
+                        wsix.v.filled[rAttr] = true;
+                    }
                     if(rAttr == dtAttr) {
                         const useClass = receivingElement.children[0].className;
                         receivingElement.children[0].className += ' d-none';
@@ -79,6 +131,10 @@ const wsix = {
                     event.preventDefault();
                 }
                 v.ondragover = ($e) => {
+                    $e.preventDefault();
+                    // window.event.preventDefault();
+                };
+                v.touchmove = ($e) => {
                     $e.preventDefault();
                     // window.event.preventDefault();
                 };
