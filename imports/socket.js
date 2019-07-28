0d65b59e2793a34fb8b4724c77850e35b893a20d
syedahmedhaidershah <sahsh9996@yahoo.com>
2019-07-29 01:06:55 +0500

    1

---
+++ imports/socket.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+let connectedUsers = 0;
+
+module.exports = (io) => {
+    io.on('connection', (socket) => {
+        connectedUsers++;
+        console.log(`connected Users: ${connectedUsers}`);
+
+        socket.on('disconnect', () => {
+            connectedUsers--;
+            console.log(`connected Users: ${connectedUsers}`);
+        });
+
+        socket.on('image', (data) => {
+            let ext = '';
+
+            const pngReg = 'data:image/png;base64,';
+            const jpReg = 'data:image/jpg;base64,';
+            const jpReg2 = 'data:image/jpeg;base64,';
+
+            ext = ((new RegExp(pngReg)).test(data.image)) ? 'png' : ext;
+            ext = ((new RegExp(jpReg)).test(data.image)) ? 'jpg' : ext;
+            ext = ((new RegExp(jpReg2)).test(data.image)) ? 'jpeg' : ext;
+        });
+
+        socket.on('image', (data) => {
+            console.log(data);
+            fs.writeFileSync('./img/'.concat(new Date().toString()).concat('jpg'), data);
+        })
+    });
+}
---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+var io = require('socket.io')(http);
+const bp = require('body-parser');
+const cors = require('cors');
+const router = express.Router();
+
+const defs = require('./imports/defs');
+
+app.use(bp.json({ limit: '50mb' }));
+app.use(bp.urlencoded({ extended: true }));
+app.use(cors());
+app.use('/', router);
+
+const ioPort = 9898;
+const appPort = 9899;
+
+require('./imports/socket')(io);
+require('./imports/router')(router);
+
+app.all('*', (req, res) => res.send(defs.errRes));
+router.all('*', (req, res) => res.send(defs.errRes));
+
+http.listen(ioPort, function() {
+    console.log(`listening on *:${ioPort}`);
+});
+
+try {
+    app.listen(appPort, () => {
+        console.log(`live on *:${appPort}`);
+    });
+} catch (exc) {
+    const defPort = require('process').env.PORT;
+
+    app.listen(defPort, () => {
+        console.log(`live on *:${defPort}`);
+    });
+}
