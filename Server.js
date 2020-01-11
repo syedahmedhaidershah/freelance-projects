4698c498925627d396e11b128109dee8fbcbc131
rizviwajahat9 <rizviwajahat9@yahoo.com>
2020-01-11 05:10:40 +0500

    asd

--- Server.js
+++ Server.js
@@ -60,7 +60,7 @@ app.get('/get_products', (req, res) => {
     });
 
 });
-app.get('/get_products_id/:id', (req, res) => {
+app.get('/get_products_id', (req, res) => {
     console.log(req.query.id);
 
     connection.query(`SELECT * FROM Products WHERE id = ${req.query.id}`, (err, rows) => {
