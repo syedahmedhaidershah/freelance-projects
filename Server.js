b6a37fe5eeb5a93af71d9d34a3d6e6de0b37c82f
rizviwajahat9 <rizviwajahat9@yahoo.com>
2020-09-20 17:30:31 +0500

    asd

--- Server.js
+++ Server.js
@@ -721,7 +721,7 @@ app.get('/getBACS', (req, res) => {
         //     // res.send(rows);
         // });
         var data = []
-        connection.query(`SELECT Stall.id , StallHolder.bankName , SUM( CAST(NewInvoiceDetails.price AS SIGNED ) ) AS total FROM Stall ,StallHolder , NewInvoiceDetails WHERE Stall.stallHolderId = StallHolder.id AND Stall.id = NewInvoiceDetails.stallId AND CAST(NewInvoiceDetails.price AS SIGNED) > 0 AND (DATE(NewInvoiceDetails.dateTime) BETWEEN '${moment().weekday(0).subtract(6,'d').format("YYYY-MM-DD")}' AND '${moment().weekday(0).format("YYYY-MM-DD")}') GROUP BY Stall.id`, (err, rows1) => {
+        connection.query(`SELECT Stall.id , StallHolder.bankName , ((1-(CAST(StallHolder.rent AS SIGNED)) / 100) * SUM( CAST(NewInvoiceDetails.price AS SIGNED ) )) AS total FROM Stall ,StallHolder , NewInvoiceDetails WHERE Stall.stallHolderId = StallHolder.id AND Stall.id = NewInvoiceDetails.stallId AND CAST(NewInvoiceDetails.price AS SIGNED) > 0 AND (DATE(NewInvoiceDetails.dateTime) BETWEEN '${moment().weekday(0).subtract(6,'d').format("YYYY-MM-DD")}' AND '${moment().weekday(0).format("YYYY-MM-DD")}') AND StallHolder.rent > 0 AND StallHolder.rent < 100 GROUP BY Stall.id`, (err, rows1) => {
             if (err) throw err;
                 if(rows1){
                  data = rows1.filter(v=> {
